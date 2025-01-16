b45f29a0d80ba3415593072b366bf76d8e950d26
Kentaro Ishikawa <53816975+Kishikawa1286@users.noreply.github.com>
2025-01-16 04:54:54 +0000

    Fix extension versions

--- .devcontainer/init.jl
+++ .devcontainer/init.jl
@@ -1,22 +1,25 @@
 using Pkg
 
-function main()
-    Pkg.add(
-        Pkg.PackageSpec[
-            Pkg.PackageSpec(name="Distributions"),
-            Pkg.PackageSpec(name="HiGHS", version="1.12.2"),
-            Pkg.PackageSpec(name="IntervalArithmetic", version="0.22.19"),
-            Pkg.PackageSpec(name="JuMP"),
-            Pkg.PackageSpec(name="LaTeXStrings"),
-            Pkg.PackageSpec(name="Plots"),
-            Pkg.PackageSpec(name="PyPlot"),
-            Pkg.PackageSpec(name="StatsPlots"),
-            Pkg.PackageSpec(name="Ipopt", version="1.6.7")
-        ]
-    )
-    Pkg.precompile()
-end
+Pkg.add("Conda")
 
-if abspath(PROGRAM_FILE) == @__FILE__
-    main()
-end
+using Conda
+
+Conda.add("python=3.11")
+Conda.add("jupyter")
+Conda.add("nbconvert")
+
+Pkg.add(
+    Pkg.PackageSpec[
+        Pkg.PackageSpec(name="Distributions"),
+        Pkg.PackageSpec(name="HiGHS", version="1.12.2"),
+        Pkg.PackageSpec(name="IntervalArithmetic", version="0.22.19"),
+        Pkg.PackageSpec(name="JuMP"),
+        Pkg.PackageSpec(name="LaTeXStrings"),
+        Pkg.PackageSpec(name="Plots"),
+        Pkg.PackageSpec(name="PyPlot"),
+        Pkg.PackageSpec(name="StatsPlots"),
+        Pkg.PackageSpec(name="Ipopt", version="1.6.7")
+    ]
+)
+
+Pkg.precompile()
